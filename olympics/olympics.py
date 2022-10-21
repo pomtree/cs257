f4bca13e7e90ea578cdcb2aa49a1e6e1f074f25e
root <root@LAPTOP-S7LT17FK.localdomain>
2022-10-20 20:44:46 -0700

    solved list NOCs by medals

--- olympics/olympics.py
+++ olympics/olympics.py
@@ -44,17 +44,45 @@ def get_athletes_by_noc(noc):
     connection.close()
     #return athletes
 
+def most_medals():
+    print("NOCs by most gold medals won (individuals, not sports):")
+    try:
+        connection = get_connection()
+        cursor = connection.cursor()
+        query = '''
+            SELECT COUNT(linking_table.medal), athlete_instances.noc
+            FROM linking_table, athlete_instances
+            WHERE linking_table.medal = 'Gold'
+            AND linking_table.athlete_instance_id = athlete_instances.id
+            GROUP BY athlete_instances.noc
+            ORDER BY COUNT(linking_table.medal) DESC'''
+        cursor.execute(query)
+        for row in cursor:
+            count = str(row[0])
+            noc = row[1]
+            print('\t' + noc + ": " + count)
+            #athletes.append(f'{given_name} {surname}')
+
+    except Exception as e:
+        print(e, file=sys.stderr)
+
+    connection.close()
+    #return athletes
+
 parser = argparse.ArgumentParser(description='##HELP STATEMENT')
 
 parser.add_argument('--noc', type=str)
+parser.add_argument('--most_medals', action='store_true')
 
 args = parser.parse_args()
 
 #List the names of all the athletes from a specified NOC.
-if(args.noc):
+if args.noc:
     #print('hello noc ' + args.noc)
     get_athletes_by_noc(args.noc)
 
+if args.most_medals:
+    most_medals()
 
 
 #print(args.accumulate(args.integers))
